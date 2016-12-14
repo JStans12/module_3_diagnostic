ba00d226b46dc6d82351fa32937dc941ab735041
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 10:55:21 -0700

    bootstrap tables: meh

--- spec/features/a_user_searches_a_zipcode_spec.rb
+++ spec/features/a_user_searches_a_zipcode_spec.rb
@@ -8,9 +8,9 @@ describe "a user navigates to the root and searches for a zipcode" do
       fill_in('location', with: 80203)
       click_on('Locate')
 
-      expect(page).to have_content("Nearest Alt Fuel Stations")
-      expect(page).to have_content("Address: 800 Acoma St")
-      expect(page).to have_content("Access Times: 24 hours daily; pay lot")
+      expect(page).to have_content("Nearest Alternative Fuel Stations")
+      expect(page).to have_content("800 Acoma St")
+      expect(page).to have_content("24 hours daily; pay lot")
       expect(page).to have_content("City of Denver - Denver Justice Center Garage")
     end
   end
