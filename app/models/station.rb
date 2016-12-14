140517842e0f7d7a471eb91343502bf58fc073c6
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 10:30:25 -0700

    refactored station model to use custom names for attributes

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,4 +1,27 @@
-class Station < OpenStruct
+class Station
+  def initialize(data)
+    @data = data
+  end
+
+  def name
+    @data[:station_name]
+  end
+
+  def address
+    @data[:street_address]
+  end
+
+  def fuel_type
+    @data[:fuel_type_code]
+  end
+
+  def access_times
+    @data[:access_days_time]
+  end
+
+  def distance
+    @data[:distance]
+  end
 
   def self.by_location(location)
     NrelService.request_stations(location).map do |station|
