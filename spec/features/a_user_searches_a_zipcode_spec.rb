2cabf2e09319d0563034a539c90e13913b88b4f2
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 10:19:08 -0700

    setup cassettes for vcr testing

--- spec/features/a_user_searches_a_zipcode_spec.rb
+++ spec/features/a_user_searches_a_zipcode_spec.rb
@@ -2,14 +2,16 @@ require 'rails_helper'
 
 describe "a user navigates to the root and searches for a zipcode" do
   it "should return top 10 results in a 6 mile radius with the appropriate fuel types" do
+    VCR.use_cassette("search by location") do
 
-    visit root_path
-    fill_in('location', with: 80203)
-    click_on('Locate')
+      visit root_path
+      fill_in('location', with: 80203)
+      click_on('Locate')
 
-    expect(page).to have_content("Nearest Alt Fuel Stations")
-    expect(page).to have_content("Address: 800 Acoma St")
-    expect(page).to have_content("Access Times: 24 hours daily; pay lot")
-    expect(page).to have_content("City of Denver - Denver Justice Center Garage")
+      expect(page).to have_content("Nearest Alt Fuel Stations")
+      expect(page).to have_content("Address: 800 Acoma St")
+      expect(page).to have_content("Access Times: 24 hours daily; pay lot")
+      expect(page).to have_content("City of Denver - Denver Justice Center Garage")
+    end
   end
 end
--- spec/models/station_spec.rb
+++ spec/models/station_spec.rb
@@ -3,15 +3,17 @@ require 'rails_helper'
 describe Station do
   context ".by_location" do
     it "returns an array of 10 station objects" do
+      VCR.use_cassette(".by_location") do
+        
+        stations = Station.by_location(80203)
+        station = stations.first
 
-      stations = Station.by_location(80203)
-      station = stations.first
-
-      expect(stations).to be_an(Array)
-      expect(stations.length).to eq(10)
-      expect(station).to be_an(Object)
-      expect(station).to respond_to(:station_name)
-      expect(station).to respond_to(:distance)
+        expect(stations).to be_an(Array)
+        expect(stations.length).to eq(10)
+        expect(station).to be_an(Object)
+        expect(station).to respond_to(:station_name)
+        expect(station).to respond_to(:distance)
+      end
     end
   end
 end
