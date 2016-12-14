1a54c58fedb2362296766dea7ef014d5171eecc7
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 09:37:11 -0700

    making successful faraday call from controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
 
   def index
-    
+    response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{params[:location]}&radius=6&fuel_type=ELEC,LPG&limit=10")
   end
 
 end
