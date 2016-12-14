0acead0af3154a5f1e3ace4c6dcc1a8fdb205c9d
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 09:30:56 -0700

    change 'q' to location

--- a_user_searches_a_zipcode_spec.rb
+++ a_user_searches_a_zipcode_spec.rb
@@ -4,7 +4,7 @@ describe "a user navigates to the root and searches for a zipcode" do
   it "should return top 10 results in a 6 mile radius with the appropriate fuel types" do
 
     visit root_path
-    fill_in('q', with: 80203)
+    fill_in('location', with: 80203)
     click_on('Locate')
 
     expect(page).to have_content("Nearest Alt Fuel Stations")
