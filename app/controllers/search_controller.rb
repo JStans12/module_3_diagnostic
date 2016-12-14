8635e2a4229da242d7fd9517388b14f56ed84d23
Joey Stansfield <joseph.r.stansfield@gmail.com>
2016-12-14 09:40:33 -0700

    successfully parsing response

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
 
   def index
     response = Faraday.get("https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV['nrel_key']}&location=#{params[:location]}&radius=6&fuel_type=ELEC,LPG&limit=10")
+    parsed = JSON.parse(response.body, symbolize_names: true)[:fuel_stations]
   end
 
 end
